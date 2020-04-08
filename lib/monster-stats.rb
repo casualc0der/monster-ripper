6326d62f6e310f976e54e0cd9fc41180fee50436
Edd Sansome <edd@murismo.com>
2020-04-08 16:54:02 +0100

    Add stat tests for monsters

--- lib/monster-stats.rb
+++ lib/monster-stats.rb
@@ -2,7 +2,8 @@ require_relative './monster_list'
 
 class Monster
   include MonsterList
-  attr_reader :name, 
+  attr_reader :name,
+              :index,
               :type, 
               :strength, 
               :dexterity, 
@@ -32,17 +33,34 @@ class Monster
     @genus
     if genus.include?(monster_type.downcase)
       @type = monster_type.downcase
+      @index = genus.find_index(type)
     else
       raise NoMethodError
     end
   end
-
   def strength
-    21
+    LIST[index][:strength]
   end
-
+  def dexterity
+    LIST[index][:dexterity]
+  end
+  def constitution
+    LIST[index][:constitution]
+  end
+  def intelligence
+    LIST[index][:intelligence]
+  end
+  def wisdom
+    LIST[index][:wisdom]
+  end
+  def charisma
+    LIST[index][:charisma]
+  end
+  def challenge_rating
+    LIST[index][:challenge_rating]
+  end
   def stats
-    {}
+  {}
   end
 end
 
--- test/monster_test.rb
+++ test/monster_test.rb
@@ -2,10 +2,11 @@ require 'minitest/autorun'
 require_relative '../lib/monster-stats'
 
 class TestMonster < Minitest::Test
-  attr_reader :monster, :default_monster
+  attr_reader :monster, :default_monster, :zombie
   def setup
     @default_monster = Monster.new("Aboleth")
     @monster = Monster.new("Gary", "Aboleth")
+    @zombie = Monster.new("Steve", "Zombie")
   end
   def test_initialize_default
     assert_equal "generic", default_monster.name
@@ -35,7 +36,52 @@ class TestMonster < Minitest::Test
   def test_stats_should_be_hash
     assert_equal true, monster.stats.is_a?(Hash)
   end
+  def test_aboleth_index_is_zero
+    assert_equal 0, monster.index
+  end
+  def test_zombie_index_is_321
+    assert_equal 321, zombie.index
+  end
   def test_aboleth_strength
     assert_equal 21, monster.strength
+  end
+  def test_zombie_strength
+    assert_equal 13, zombie.strength
+  end
+  def test_aboleth_dexterity
+    assert_equal 9, monster.dexterity
+  end
+  def test_zombie_dexterity
+    assert_equal 6, zombie.dexterity
+  end
+  def test_aboleth_constitution
+    assert_equal 15, monster.constitution
+  end
+  def test_zombie_constitution
+    assert_equal 16, zombie.constitution
+  end
+  def test_aboleth_intelligence
+    assert_equal 18, monster.intelligence
+  end
+  def test_zombie_intelligence
+    assert_equal 3, zombie.intelligence
+  end
+  def test_aboleth_wisdom
+    assert_equal 15, monster.wisdom
+  end
+  def test_zombie_wisdom
+    assert_equal 6, zombie.wisdom
+  end
+  def test_aboleth_charisma
+    assert_equal 18, monster.charisma
+  end
+  def test_zombie_charisma
+    assert_equal 5, zombie.charisma
+  end
+  def test_aboleth_challenge_rating
+    assert_equal 10, monster.challenge_rating
+  end
+  def test_zombie_challenge_rating
+    assert_equal 0.25, zombie.challenge_rating
   end
 end
