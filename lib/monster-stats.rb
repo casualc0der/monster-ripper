fb9a1f75e81c40117856b04304674fc225a521b9
Edd Sansome <edd@murismo.com>
2020-04-08 21:08:01 +0100

    refactor Monster init method

--- lib/monster-stats.rb
+++ lib/monster-stats.rb
@@ -31,46 +31,34 @@ class Monster
   
   def initialize(name="generic", monster_type)
     @name = name
-    @genus
+    
     if genus.include?(monster_type.downcase)
       @type = monster_type.downcase
       @index = genus.find_index(type)
-      @hit_points = LIST[index][:hit_points]
+      monster_index = LIST[index]
+      
+      @hit_points = monster_index[:hit_points]
+      @strength = monster_index[:strength]
+      @dexterity = monster_index[:dexterity]
+      @constitution = monster_index[:constitution]
+      @intelligence = monster_index[:intelligence]
+      @wisdom = monster_index[:wisdom]
+      @charisma = monster_index[:charisma]
+      @challenge_rating = monster_index[:challenge_rating]
     else
       raise NoMethodError
     end
-  end
-  def strength
-    LIST[index][:strength]
-  end
-  def dexterity
-    LIST[index][:dexterity]
-  end
-  def constitution
-    LIST[index][:constitution]
-  end
-  def intelligence
-    LIST[index][:intelligence]
-  end
-  def wisdom
-    LIST[index][:wisdom]
-  end
-  def charisma
-    LIST[index][:charisma]
-  end
-  def challenge_rating
-    LIST[index][:challenge_rating]
   end
   def stats
   {}
   end
 end
+p Monster.genus_available
+# hordeOfZombies =  [Monster.new('gary', 'zombie'), Monster.new('sam','zombie'), Monster.new('david','zombie')]
 
-hordeOfZombies =  [Monster.new('gary', 'zombie'), Monster.new('sam','zombie'), Monster.new('david','zombie')]
-
-p hordeOfZombies[0].hit_points
-hordeOfZombies[0].hit_points -= 23
-p hordeOfZombies[0].hit_points
-p hordeOfZombies[1].hit_points
+# p hordeOfZombies[0].hit_points
+# hordeOfZombies[0].hit_points -= 23
+# p hordeOfZombies[0].hit_points
+# p hordeOfZombies[1].hit_points
 
 
